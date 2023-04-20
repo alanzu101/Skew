5982f3384a65764df438222ed69551138a6881a4
Alan Zu <alan101@berkeley.edu>
2023-04-20 01:15:54 -0700

    edited SkewManager.h

--- protocol/Skew/SkewManager.h
+++ protocol/Skew/SkewManager.h
@@ -1,22 +1,23 @@
 //
-// Created by Yi Lu on 9/13/18.
+// Created by Yi Lu
+// Modified by Alan Zu
 //
 
 #pragma once
 
 #include "core/Manager.h"
-#include "protocol/Calvin/Calvin.h"
-#include "protocol/Calvin/CalvinExecutor.h"
-#include "protocol/Calvin/CalvinHelper.h"
-#include "protocol/Calvin/CalvinPartitioner.h"
-#include "protocol/Calvin/CalvinTransaction.h"
+#include "protocol/Skew/SkewExecutor.h"
+#include "protocol/Skew/SkewHelper.h"
+#include "protocol/Skew/SkewPartitioner.h"
+#include "protocol/Skew/SkewTransaction.h"
+#include "protocol/Skew/SkewScheduleMap.h"
 
 #include <thread>
 #include <vector>
 
 namespace aria {
 
-template <class Workload> class CalvinManager : public aria::Manager {
+template <class Workload> class SkewManager : public aria::Manager {
 public:
   using base_type = aria::Manager;
 
@@ -24,34 +25,51 @@ public:
   using DatabaseType = typename WorkloadType::DatabaseType;
   using StorageType = typename WorkloadType::StorageType;
 
-  using TransactionType = CalvinTransaction;
+  using TransactionType = SkewTransaction;
   static_assert(std::is_same<typename WorkloadType::TransactionType,
                              TransactionType>::value,
                 "Transaction types do not match.");
   using ContextType = typename DatabaseType::ContextType;
   using RandomType = typename DatabaseType::RandomType;
 
-  CalvinManager(std::size_t coordinator_id, std::size_t id, DatabaseType &db,
+  SkewManager(std::size_t coordinator_id, std::size_t id, DatabaseType &db,
                 const ContextType &context, std::atomic<bool> &stopFlag)
       : base_type(coordinator_id, id, context, stopFlag), db(db),
         partitioner(coordinator_id, context.coordinator_num,
-                    CalvinHelper::string_to_vint(context.replica_group)) {
+                    SkewHelper::string_to_vint(context.replica_group)),
+        n_scheduler(SkewHelper::n_scheduler(partitioner.replica_group_id, id,
+            SkewHelper::string_to_vint(context.scheduler))) {
 
     storages.resize(context.batch_size);
     transactions.resize(context.batch_size);
+    schedule_maps.resize(n_scheduler);
+
+    // LOG(INFO)<<"context.scheduler = " << context.scheduler <<std::endl;
+    // LOG(INFO)<<"context.replica_group = "<< context.replica_group <<std::endl;
+    // LOG(INFO)<<"n_scheduler = " << n_scheduler << "; partitioner.replica_group_id = " << partitioner.replica_group_id<<std::endl;
+    // LOG(INFO)<<"partitioner.replica_group_size = "<< partitioner.replica_group_size <<std::endl;
   }
 
   void coordinator_start() override {
 
     std::size_t n_workers = context.worker_num;
     std::size_t n_coordinators = context.coordinator_num;
-
+    
+    batch_count.store(0);
     while (!stopFlag.load()) {
+
+      // LOG(INFO)<< "---- manager(" << this->id << ") is signaling to start T batch : " << batch_count.load() << std::endl;
 
       // the coordinator on each machine generates
       // a batch of transactions using the same random seed.
-
       // LOG(INFO) << "Seed: " << random.get_seed();
+
+      // initialize schedule_maps 
+      if (batch_count.load() == 0 || !context.same_batch) {
+        for (auto scheduler_id = 0u; scheduler_id < n_scheduler; scheduler_id++) {
+          schedule_maps[scheduler_id] = ScheduleMapFactory::create_schedule_map();
+        }
+      }
       n_started_workers.store(0);
       n_completed_workers.store(0);
       signal_worker(ExecutorStatus::Analysis);
@@ -63,19 +81,19 @@ public:
       // wait for all machines until they finish the analysis phase.
       wait4_ack();
 
-      // Allow each worker to run transactions
-      // DB is partitioned by the number of lock managers.
-      // The first k workers act as lock managers to grant locks to other
-      // workers The remaining workers run transactions upon assignment via the
-      // queue.
+      // LOG(INFO)<< "---- manager(" << this->id << ") is signaling to enter EXECUTE_PHASE for T batch : " << batch_count << std::endl;
+
+      // Allow each worker to run transactions upon assignment via the queue.
       n_started_workers.store(0);
       n_completed_workers.store(0);
-      clear_lock_manager_status();
+      clear_scheduler_status();
       signal_worker(ExecutorStatus::Execute);
       wait_all_workers_start();
       wait_all_workers_finish();
       // wait for all machines until they finish the execution phase.
       wait4_ack();
+
+      batch_count++;
     }
 
     signal_worker(ExecutorStatus::EXIT);
@@ -85,9 +103,18 @@ public:
 
     std::size_t n_workers = context.worker_num;
     std::size_t n_coordinators = context.coordinator_num;
-
+  
+    batch_count.store(0);
     for (;;) {
       // LOG(INFO) << "Seed: " << random.get_seed();
+
+      // initialize schedule_maps 
+      if (batch_count.load() == 0 || !context.same_batch) {
+        for (auto scheduler_id = 0u; scheduler_id < n_scheduler; scheduler_id++) {
+          schedule_maps[scheduler_id] = ScheduleMapFactory::create_schedule_map();
+        }
+      }
+      
       ExecutorStatus status = wait4_signal();
       if (status == ExecutorStatus::EXIT) {
         set_worker_status(ExecutorStatus::EXIT);
@@ -110,36 +137,35 @@ public:
 
       status = wait4_signal();
       DCHECK(status == ExecutorStatus::Execute);
-      // Allow each worker to run transactions
-      // DB is partitioned by the number of lock managers.
-      // The first k workers act as lock managers to grant locks to other
-      // workers The remaining workers run transactions upon assignment via the
-      // queue.
+      // Allow each worker to run transactions upon assignment via the queue.
       n_started_workers.store(0);
       n_completed_workers.store(0);
-      clear_lock_manager_status();
+      clear_scheduler_status();
       set_worker_status(ExecutorStatus::Execute);
       wait_all_workers_start();
       wait_all_workers_finish();
       send_ack();
+
+      batch_count.fetch_add(1);
     }
   }
 
-  void add_worker(const std::shared_ptr<CalvinExecutor<WorkloadType>>
-
-                      &w) {
+  void add_worker(const std::shared_ptr<SkewExecutor<WorkloadType>> &w) {
     workers.push_back(w);
   }
 
-  void clear_lock_manager_status() { lock_manager_status.store(0); }
+  void clear_scheduler_status() { scheduler_status.store(0); }
 
 public:
   RandomType random;
   DatabaseType &db;
-  CalvinPartitioner partitioner;
-  std::atomic<uint32_t> lock_manager_status;
-  std::vector<std::shared_ptr<CalvinExecutor<WorkloadType>>> workers;
+  SkewPartitioner partitioner;
+  std::atomic<uint32_t> scheduler_status;
+  std::vector<std::shared_ptr<SkewExecutor<WorkloadType>>> workers;
   std::vector<StorageType> storages;
   std::vector<std::unique_ptr<TransactionType>> transactions;
+  std::size_t n_scheduler;
+  std::vector<std::unique_ptr<SkewScheduleMap>> schedule_maps;
+  std::atomic<uint32_t> batch_count;
 };
 } // namespace aria
